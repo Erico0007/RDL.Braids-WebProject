7b1eb043ec694a216a565c4fc121ae6b896c5be6
Erico0007 <168248689+Erico0007@users.noreply.github.com>
2025-08-16 10:13:32 -0400

    I  imporve the website by adding  new style

---
+++ Scripts/Doc.js
@@ -0,0 +1,8 @@
+console.log("Doc.js loaded");
+document.addEventListener("DOMContentLoaded", function () {
+  const serviceList = document.querySelector(".service-list");
+  serviceList.style.display = "flex";
+  serviceList.style.flexWrap = "wrap";
+  serviceList.style.justifyContent = "center";
+});
+console.log("Doc.js executed");
